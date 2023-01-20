6e83e09a09cda310ab0158305c85e7f6dfca234b
Hoang Linh <hlinh.jobs@gmail.com>
2023-01-20 21:17:07 +0700

    init project

---
+++ WeatherApp/WeatherApp/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  WeatherApp
+//
+//  Created by Hoang Linh Nguyen on 20/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
